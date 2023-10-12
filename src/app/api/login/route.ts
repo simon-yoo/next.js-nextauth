722a6e066ab608bf076578950d4432eaeda8d780
Simon Yoo <yoosghn0@gmail.com>
2023-10-12 19:33:22 +1300

    nvm

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,21 @@
+import prisma from '../../../../lib/prisma';
+import * as bcrypt from 'bcrypt';
+interface RequestBody {
+  username: string;
+  password: string;
+}
+
+export async function Post(request: Request) {
+  const body: RequestBody = await request.json();
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email: body.username,
+    },
+  });
+
+  if (user && (await bcrypt.compare(body.password, user.password))) {
+    const { password, ...userWithoutPass } = user;
+    return new Response(JSON.stringify(userWithoutPass));
+  }
+}
