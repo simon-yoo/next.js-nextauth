e1e11aef04ea90e72780b57fe307df49bcad210d
Simon Yoo <yoosghn0@gmail.com>
2023-10-16 11:25:46 +1300

    provided authorization for the header with valid access token

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,9 +1,22 @@
+import { verifyJwt } from '../../../../../lib/jwt';
 import prisma from '../../../../../lib/prisma';
 
 export async function GET(
   request: Request,
   { params }: { params: { id: number } }
 ) {
+  // checks if the access token is inside the header of the request
+  const accesTolken = request.headers.get('authorization');
+  if (!accesTolken || !verifyJwt(accesTolken)) {
+    return new Response(
+      JSON.stringify({
+        error: 'unauthorized',
+      }),
+      {
+        status: 401,
+      }
+    );
+  }
   const userPosts = await prisma.post.findMany({
     where: { authorId: +params.id },
     include: {
